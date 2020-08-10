302808a062e8c06ef472904207bcdd74ff6dbb7b
brookebachman00 <bbachman1994@gmail.com>
2020-08-10 15:35:40 -0600

    having fun with fonts and working on the about page

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        
+            <div id="about-container">
+            <h1 id="paige-bio">
+                My name is Paige. My pronouns are they/them, I am a healer. I am able to help women overcome trauma.
+            </h1>
+            
+        </div>
+    )
+}
+
+export default About
